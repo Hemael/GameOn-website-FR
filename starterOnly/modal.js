4710a3c908e46c68b91acc0ceb2c6d0387fdd51c
Henele <chloe.legouas@hotmail.fr>
2022-06-26 15:07:02 +0200

    avancer verification

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,62 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeBtn = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", () => {changeDisplayModal("block")}));
+closeBtn.forEach((btn) => btn.addEventListener("click", () => {changeDisplayModal("none")}));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+
+function changeDisplayModal(displayChange){
+  console.log(modalbg.style.display);
+  modalbg.style.display = displayChange;
 }
 
 
+function validate(e){
+  let errors = [];
+  let prenom = document.getElementsByName("first")[0].value;
+  let nom = document.getElementsByName("last")[0].value;
+
+  if(prenom.length < 2){
+    errors.push("entrez un prénom correct")
+  }
+  if(nom.length < 2){
+    errors.push("entrez un nom correct")
+  }
+  if(errors.length > 0){
+    e.preventDefault();
+  }
+  
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // launch modal form
+// function launchModal() {
+//   modalbg.style.display = "block";
+// }
+
+// function closeModal() {
+//   modalbg.style.display = "none";
+//}
